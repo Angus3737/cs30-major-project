bffc0f7f3ba839b7f5af245191947e3c796fb361
Li5604976 <5604976@spsd.sk.ca>
2025-04-29 13:18:59 -0600

    added grid based game starting point

---
+++ sketch.js
@@ -0,0 +1,255 @@
+// Chinese Chess
+// Angus Li
+// April 10, 2025
+//
+// Extra for Experts:
+//- Learned to use ternary operator in clearPath function
+//- Used paint.net to edit the designs of the pieces
+//- Added an html link for instructions
+
+const CELL_SIZE = 80;
+let rows = 9;
+let cols = 9;
+let pieceSelected = false;
+let redKing;
+let greenKing;
+let selectedX = -1;
+let selectedY = -1;
+let selectedPieceType = 0;
+let state = "redTurn";
+let winner;
+
+let board = [
+  ['c', 'c', 'c', 'c', 'k', 'c', 'c', 'c', 'c'],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ['rc', 'rc', 'rc', 'rc', 'rk', 'rc', 'rc', 'rc', 'rc']
+];
+
+function preload() {
+  //load images for pieces and river
+  redKing = loadImage("redking.png");
+  blackKing = loadImage("blackking.png");
+  redChariot = loadImage("redchariot.png");
+  blackChariot = loadImage("blackchariot.png");
+
+  water = loadImage("blue-water.avif");
+}
+
+function setup() {
+  createCanvas(CELL_SIZE * cols, CELL_SIZE * rows);
+
+  //creates link to "how to play"
+  let link = createA('https://www.ymimports.com/pages/how-to-play-xiangqi-chinese-chess', 'How to Play');
+  link.position(-200, 200);
+
+  //more info in my version
+  p = createP('The chariots move the same as rooks');
+  p.position(-400, 0);
+
+  p1 = createP('In this version, the river is just for design');
+  p1.position(-400, 50);
+}
+
+function draw() {
+  background(220);
+
+  //show "game over" message after someone wins
+  if (state === "gameOver") {
+    textAlign(CENTER, CENTER);
+    textSize(32);
+    fill("black");
+    text(winner, width / 2, height / 2);
+    return;
+  }
+
+  displayGrid();
+  displayRiver();
+  displayPieces();
+}
+
+function displayGrid() {
+
+  // draws a grid of 9 x 9
+  fill(247, 219, 167);
+  for (let x = 0; x < cols; x++) {
+    for (let y = 0; y < rows; y++) {
+      square(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE);
+    }
+  }
+}
+
+function displayRiver() {
+  //draws river just for the background
+  for (let x = 0; x < cols; x++) {
+    image(water, x * CELL_SIZE, 4 * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+  }
+}
+
+function displayPieces() {
+
+  //displays pieces using images
+  for (let x = 0; x < cols; x++) {
+    for (let y = 0; y < rows; y++) {
+      if (board[y][x] === 'rc') {
+        image(redChariot, x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+      }
+
+      else if (board[y][x] === 'c') {
+        image(blackChariot, x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+      }
+
+      else if (board[y][x] === 'rk') {
+        image(redKing, x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+      }
+
+      else if (board[y][x] === 'k') {
+        image(blackKing, x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+      }      
+    }
+  }
+}
+
+function mousePressed() {
+
+  //piece selection and piece capturing
+  let x = Math.floor(mouseX/CELL_SIZE);
+  let y = Math.floor(mouseY/CELL_SIZE);
+  if (x >= 0 && x < cols && y >= 0 && y < rows) {
+    let clickedPiece = board[y][x];
+
+    if (pieceSelected) {
+      let pieceMoved = movePiece(selectedX, selectedY, x, y);
+      pieceSelected = false;
+
+      //alternate turns if moved and if king wasn't captured
+      if (pieceMoved) {
+        checkForWin();
+        if (state !== "gameOver") {
+          if (state === "redTurn") {
+            state = "blackTurn";
+          }
+          else if (state === "blackTurn") {
+            state = "redTurn";
+          }
+        }
+      }
+    }
+    else if (clickedPiece !== 0) {
+
+      //selects a piece if it's a player's turn
+      if (state === "redTurn" && clickedPiece.startsWith("r") || 
+      state === "blackTurn" && !clickedPiece.startsWith("r")) {
+        selectedX = x;
+        selectedY = y;
+        selectedPieceType = clickedPiece;
+        pieceSelected = true;
+      }
+    }
+  }
+}
+
+function movePiece(oldX, oldY, newX, newY) {
+
+  //moves a piece only if move is legal
+  let piece = board[oldY][oldX];
+  let targetPiece = board[newY][newX];
+
+  //if piece is a king, it can only move 1 square
+  if (piece === 'rk'|| piece === 'k') {
+    if (!(Math.abs(oldX - newX) <= 1 && Math.abs(oldY - newY) <= 1)) {
+      return false;
+    }
+  }
+
+  //can only move horizontally and veritcally
+  if (!(oldX === newX || oldY === newY)) {
+    return false;
+  }
+
+  //checks for pieces blocking path
+  if (!clearPath(oldX, oldY, newX, newY)) {
+    return false;
+  }
+
+  //can't capture your own piece
+  if (targetPiece !== 0 && sameTeam(piece, targetPiece)) {
+    return false;
+  }
+
+  //moves the piece
+  board[newY][newX] = piece;
+  board[oldY][oldX] = 0;
+  return true;
+}
+
+function clearPath(oldX, oldY, newX, newY) {
+
+  //checks for piece blocking the path for a move
+
+  //vertical movements
+  if (oldX === newX) {
+    let step = newY > oldY ? 1 : -1;
+    for (let y = oldY + step; y !== newY; y += step) {
+      if (board[y][oldX] !== 0) {
+        return false;
+      }
+    }
+  }
+
+  //horizontal movements
+  else if (oldY === newY) {
+    let step = newX > oldX ? 1 : -1;
+    for (let x = oldX + step; x !== newX; x += step) {
+      if (board[oldY][x] !== 0) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+function sameTeam(piece1, piece2) {
+
+  //checks if 2 pieces are on the same team
+  if (piece1.startsWith('r') && piece2.startsWith('r')) {
+    return true;
+  }
+  if (!piece1.startsWith('r') && !piece2.startsWith('r')) {
+    return true;
+  }
+  return false;
+}
+
+function checkForWin() {
+
+  //checks if any of the kings are still alive
+  let redKingAlive = false;
+  let blackKingAlive = false;
+
+  for (let y = 0; y < rows; y++) {
+    for (let x = 0; x < cols; x++) {
+      if (board[y][x] === "rk") {
+        redKingAlive = true;
+      }
+      else if (board[y][x] === "k") {
+        blackKingAlive = true;
+      }
+    }
+  }
+
+  if (!redKingAlive) {
+    state = "gameOver";
+    winner = "Black Wins!!!";
+  }
+
+  else if (!blackKingAlive) {
+    state = "gameOver";
+    winner = "Red Wins!!!";
+  }
+}
